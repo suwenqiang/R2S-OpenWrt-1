be1b0812b7afd39f8c4e671ce2ab85cb94e1dd78
nicksun98 <44309749+nicksun98@users.noreply.github.com>
2020-09-12 13:40:30 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -28,7 +28,7 @@ rm -rf ./package/network/config/firewall
 svn co https://github.com/openwrt/openwrt/branches/openwrt-19.07/package/network/config/firewall package/network/config/firewall
 #使用19.07的feed源
 rm -f ./feeds.conf.default
-wget https://raw.githubusercontent.com/nicksun98/openwrt/openwrt-19.07/feeds.conf.default
+wget https://raw.githubusercontent.com/openwrt/openwrt/openwrt-19.07/feeds.conf.default
 wget -P include/ https://raw.githubusercontent.com/openwrt/openwrt/openwrt-19.07/include/scons.mk
 patch -p1 < ../PATCH/new/main/0001-tools-add-upx-ucl-support.patch
 #SWAP LAN WAN
@@ -54,7 +54,7 @@ sed -i 's/-f/-f -i/g' feeds/packages/utils/rng-tools/files/rngd.init
 #patch rk-crypto
 patch -p1 < ../PATCH/new/main/kernel_crypto-add-rk3328-crypto-support.patch
 #luci network
-#patch -p1 < ../PATCH/new/main/luci_network-add-packet-steering.patch
+patch -p1 < ../PATCH/new/main/luci_network-add-packet-steering.patch
 #patch jsonc
 patch -p1 < ../PATCH/new/package/use_json_object_new_int64.patch
 #patch dnsmasq
@@ -269,7 +269,7 @@ cp -rf ../openwrt-lienol/package/network/fullconenat ./package/network/fullconen
 MY_Var=package/lean/lean-translate
 git clone -b master --single-branch https://github.com/QiuSimons/addition-trans-zh ${MY_Var}
 sed -i '/uci .* dhcp/d' ${MY_Var}/files/zzz-default-settings
-sed -i '/chinadnslist\|ddns\|openwrt_luci\|upnp\|netease\|openclash\|dockerman/d' ${MY_Var}/files/zzz-default-settings
+sed -i '/chinadnslist\|ddns\|upnp\|netease\|openclash\|dockerman/d' ${MY_Var}/files/zzz-default-settings
 sed -i "4a uci set luci.main.lang='en'" ${MY_Var}/files/zzz-default-settings
 sed -i '5a uci commit luci' ${MY_Var}/files/zzz-default-settings
 sed -i '/^[[:space:]]*$/d' ${MY_Var}/files/zzz-default-settings
